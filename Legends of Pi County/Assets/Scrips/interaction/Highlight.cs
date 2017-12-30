d71f5806287b235a9d9d373415296460f484f805
Shikasar7 <Shikasar@gmail.com>
2017-12-30 21:45:22 +0800

    单位选中判定12

---
+++ Legends of Pi County/Assets/Scrips/interaction/Highlight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : interaction {
+	public GameObject DisplayItem;
+	public override void Deselect ()
+	{
+		DisplayItem.SetActive (false);
+	}
+	public override void Select ()
+	{
+    DisplayItem.SetActive (true);
+	}
+	// Use this for initialization
+	void Start () {
+		DisplayItem.SetActive (false);
+	}
+
+	
+
+}
