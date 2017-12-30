7fce44e89753c611eb45d67346ae50d05bfa3449
Shikasar7 <Shikasar@gmail.com>
2017-12-30 19:10:09 +0800

    单位选中判定

---
+++ Legends of Pi County/Assets/Scrips/interactions/interaction.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class interaction : MonoBehaviour {
+
+	public abstract void Selecet ();
+	public abstract void Deselect ();
+																										
+}
