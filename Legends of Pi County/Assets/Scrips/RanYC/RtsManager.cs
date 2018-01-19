10fd95982ea71351a362e5d5d9a314c7d1431f8f
2855239858 <2855239858@qq.com>
2018-01-19 14:08:23 +0800

    冉运川-地图方面第一次上次

---
+++ Legends of Pi County/Assets/Scrips/RanYC/RtsManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RtsManager : MonoBehaviour {
+
+    public static RtsManager Current = null;
+
+	// Use this for initialization
+	void Start () {
+        Current = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
