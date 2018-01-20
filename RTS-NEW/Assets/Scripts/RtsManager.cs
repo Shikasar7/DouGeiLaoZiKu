0382c526ed48ec6a3f582c8e1833d799ff522b2c
2855239858 <2855239858@qq.com>
2018-01-20 20:58:45 +0800

    第二次上次
    我重新做了一个地图，按照教程做到了任务12

---
+++ RTS-NEW/Assets/Scripts/RtsManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RtsManager : MonoBehaviour {
+
+    public static RtsManager Current = null;
+
+    public List<PlayerSetupDefinition> Players = new List<PlayerSetupDefinition>();
+	// Use this for initialization
+	void Start () {
+        Current = this;
+        foreach(var p in Players)
+        {
+            foreach(var u in p.StartingUnits)
+            {
+                GameObject.Instantiate(u, p.Location.position, p.Location.rotation);
+            }
+        }
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
